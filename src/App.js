33119f47fdf4623907965477f77671ff2cab4bf5
Pizhayut Watanasirichaikul <pizhayut@users.noreply.github.com>
2021-11-11 00:16:09 +0700

    passing function with context

--- src/App.js
+++ src/App.js
@@ -35,9 +35,10 @@ function App() {
     <AuthContext.Provider
       value={{
         isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler
       }}
     >
-      <MainHeader onLogout={logoutHandler} />
+      <MainHeader />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
